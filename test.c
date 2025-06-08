4153ef8c4bf6c2877ddc6225e1193c71c783ce1a
Al <albarrentine@gmail.com>
2025-06-08 02:16:36 -0400

    [test] adding tests for aligned and removing dependencies from Makefile

--- test.c
+++ test.c
@@ -1,5 +1,6 @@
 #include "greatest/greatest.h"
 #include "cstring_array.h"
+#include "cstring_array_aligned.h"
 
 TEST test_cstring_array_new(void) {
     cstring_array *array = cstring_array_new();
@@ -7,6 +8,15 @@ TEST test_cstring_array_new(void) {
     ASSERT_EQ(array->indices->n, 0);
     ASSERT_EQ(array->str->n, 0);
     cstring_array_destroy(array);
+    PASS();
+}
+
+TEST test_cstring_array_aligned_new(void) {
+    cstring_array_aligned *array = cstring_array_aligned_new();
+    ASSERT(array != NULL);
+    ASSERT_EQ(array->indices->n, 0);
+    ASSERT_EQ(array->str->n, 0);
+    cstring_array_aligned_destroy(array);
     PASS();
 }
 
@@ -19,10 +29,26 @@ TEST test_cstring_array_new_size(void) {
     PASS();
 }
 
+TEST test_cstring_array_aligned_new_size(void) {
+    cstring_array_aligned *array = cstring_array_aligned_new_size(10);
+    ASSERT(array != NULL);
+    ASSERT_EQ(array->indices->n, 0);
+    ASSERT_EQ(array->str->m, 10);
+    cstring_array_aligned_destroy(array);
+    PASS();
+}
+
 TEST test_cstring_array_capacity(void) {
     cstring_array *array = cstring_array_new_size(10);
     ASSERT_EQ(cstring_array_capacity(array), 10);
     cstring_array_destroy(array);
+    PASS();
+}
+
+TEST test_cstring_array_aligned_capacity(void) {
+    cstring_array_aligned *array = cstring_array_aligned_new_size(10);
+    ASSERT_EQ(cstring_array_aligned_capacity(array), 10);
+    cstring_array_aligned_destroy(array);
     PASS();
 }
 
@@ -33,10 +59,24 @@ TEST test_cstring_array_used(void) {
     PASS();
 }
 
+TEST test_cstring_array_aligned_used(void) {
+    cstring_array_aligned *array = cstring_array_aligned_new_size(10);
+    ASSERT_EQ(cstring_array_aligned_used(array), 0);
+    cstring_array_aligned_destroy(array);
+    PASS();
+}
+
 TEST test_cstring_array_num_strings(void) {
     cstring_array *array = cstring_array_new_size(10);
     ASSERT_EQ(cstring_array_num_strings(array), 0);
     cstring_array_destroy(array);
+    PASS();
+}
+
+TEST test_cstring_array_aligned_num_strings(void) {
+    cstring_array_aligned *array = cstring_array_aligned_new_size(10);
+    ASSERT_EQ(cstring_array_aligned_num_strings(array), 0);
+    cstring_array_aligned_destroy(array);
     PASS();
 }
 
@@ -45,6 +85,14 @@ TEST test_cstring_array_resize(void) {
     cstring_array_resize(array, 20);
     ASSERT_EQ(array->str->m, 20);
     cstring_array_destroy(array);
+    PASS();
+}
+
+TEST test_cstring_array_aligned_resize(void) {
+    cstring_array_aligned *array = cstring_array_aligned_new_size(10);
+    cstring_array_aligned_resize(array, 20);
+    ASSERT_EQ(array->str->m, 20);
+    cstring_array_aligned_destroy(array);
     PASS();
 }
 
@@ -57,6 +105,15 @@ TEST test_cstring_array_clear(void) {
     PASS();
 }
 
+TEST test_cstring_array_aligned_clear(void) {
+    cstring_array_aligned *array = cstring_array_aligned_new_size(10);
+    cstring_array_aligned_clear(array);
+    ASSERT_EQ(array->str->n, 0);
+    ASSERT_EQ(array->indices->n, 0);
+    cstring_array_aligned_destroy(array);
+    PASS();
+}
+
 TEST test_cstring_array_start_token(void) {
     cstring_array *array = cstring_array_new_size(10);
     uint32_t index = cstring_array_start_token(array);
@@ -66,12 +123,30 @@ TEST test_cstring_array_start_token(void) {
     PASS();
 }
 
+TEST test_cstring_array_aligned_start_token(void) {
+    cstring_array_aligned *array = cstring_array_aligned_new_size(10);
+    uint32_t index = cstring_array_aligned_start_token(array);
+    ASSERT_EQ(index, 0);
+    ASSERT_EQ(array->indices->n, 1);
+    cstring_array_aligned_destroy(array);
+    PASS();
+}
+
 TEST test_cstring_array_terminate(void) {
     cstring_array *array = cstring_array_new_size(10);
     cstring_array_start_token(array);
     cstring_array_terminate(array);
     ASSERT_EQ(array->str->a[0], '\0');
     cstring_array_destroy(array);
+    PASS();
+}
+
+TEST test_cstring_array_aligned_terminate(void) {
+    cstring_array_aligned *array = cstring_array_aligned_new_size(10);
+    cstring_array_aligned_start_token(array);
+    cstring_array_aligned_terminate(array);
+    ASSERT_EQ(array->str->a[0], '\0');
+    cstring_array_aligned_destroy(array);
     PASS();
 }
 
@@ -86,6 +161,17 @@ TEST test_cstring_array_add_string(void) {
     PASS();
 }
 
+TEST test_cstring_array_aligned_add_string(void) {
+    cstring_array_aligned *array = cstring_array_aligned_new_size(10);
+    uint32_t index = cstring_array_aligned_add_string(array, "hello");
+    ASSERT_EQ(index, 0);
+    ASSERT_EQ(array->indices->n, 1);
+    ASSERT_EQ(array->str->n, 6);
+    ASSERT_EQ(array->str->a[5], '\0');
+    cstring_array_aligned_destroy(array);
+    PASS();
+}
+
 TEST test_cstring_array_add_string_len(void) {
     cstring_array *array = cstring_array_new_size(10);
     uint32_t index = cstring_array_add_string_len(array, "hello", 5);
@@ -94,6 +180,17 @@ TEST test_cstring_array_add_string_len(void) {
     ASSERT_EQ(array->str->n, 6);
     ASSERT_EQ(array->str->a[5], '\0');
     cstring_array_destroy(array);
+    PASS();
+}
+
+TEST test_cstring_array_aligned_add_string_len(void) {
+    cstring_array_aligned *array = cstring_array_aligned_new_size(10);
+    uint32_t index = cstring_array_aligned_add_string_len(array, "hello", 5);
+    ASSERT_EQ(index, 0);
+    ASSERT_EQ(array->indices->n, 1);
+    ASSERT_EQ(array->str->n, 6);
+    ASSERT_EQ(array->str->a[5], '\0');
+    cstring_array_aligned_destroy(array);
     PASS();
 }
 
@@ -108,6 +205,17 @@ TEST test_cstring_array_cat_string(void) {
     PASS();
 }
 
+TEST test_cstring_array_aligned_cat_string(void) {
+    cstring_array_aligned *array = cstring_array_aligned_new_size(10);
+    cstring_array_aligned_start_token(array);
+    cstring_array_aligned_cat_string(array, "hello");
+    ASSERT_EQ(array->indices->n, 1);
+    ASSERT_EQ(array->str->n, 6);
+    ASSERT_EQ(array->str->a[5], '\0');
+    cstring_array_aligned_destroy(array);
+    PASS();
+}
+
 TEST test_cstring_array_cat_string_len(void) {
     cstring_array *array = cstring_array_new_size(10);
     cstring_array_start_token(array);
@@ -116,6 +224,17 @@ TEST test_cstring_array_cat_string_len(void) {
     ASSERT_EQ(array->str->n, 6);
     ASSERT_EQ(array->str->a[5], '\0');
     cstring_array_destroy(array);
+    PASS();
+}
+
+TEST test_cstring_array_aligned_cat_string_len(void) {
+    cstring_array_aligned *array = cstring_array_aligned_new_size(10);
+    cstring_array_aligned_start_token(array);
+    cstring_array_aligned_cat_string_len(array, "hello", 5);
+    ASSERT_EQ(array->indices->n, 1);
+    ASSERT_EQ(array->str->n, 6);
+    ASSERT_EQ(array->str->a[5], '\0');
+    cstring_array_aligned_destroy(array);
     PASS();
 }
 
@@ -135,6 +254,22 @@ TEST test_cstring_array_from_char_array(void) {
     PASS();
 }
 
+TEST test_cstring_array_aligned_from_char_array(void) {
+    char_array_aligned *str = char_array_aligned_new();
+    char_array_aligned_push(str, 'h');
+    char_array_aligned_push(str, 'e');
+    char_array_aligned_push(str, 'l');
+    char_array_aligned_push(str, 'l');
+    char_array_aligned_push(str, 'o');
+    char_array_aligned_push(str, '\0');
+    cstring_array_aligned *array = cstring_array_aligned_from_char_array(str);
+    ASSERT_EQ(array->indices->n, 1);
+    ASSERT_EQ(array->str->n, 6);
+    ASSERT_EQ(array->str->a[5], '\0');
+    cstring_array_aligned_destroy(array);
+    PASS();
+}
+
 TEST test_cstring_array_from_strings(void) {
     cstring_array *array = cstring_array_from_strings((char *[]){"hello", "world"}, 2);
     ASSERT_EQ(array->indices->n, 2);
@@ -144,50 +279,47 @@ TEST test_cstring_array_from_strings(void) {
     PASS();
 }
 
-TEST test_cstring_array_foreach(void) {
-    cstring_array *array = cstring_array_new_size(10);
-    cstring_array_add_string(array, "hello");
-    cstring_array_add_string(array, "world");
-    cstring_array_add_string(array, "foo");
-    cstring_array_add_string(array, "bar");
-
-    int i = 0;
-    char *s = NULL;
-    cstring_array_foreach(array, i, s, {
-        if (i == 0) {
-            ASSERT_STR_EQ(s, "hello");
-        } else if (i == 1) {
-            ASSERT_STR_EQ(s, "world");
-        } else if (i == 2) {
-            ASSERT_STR_EQ(s, "foo");
-        } else if (i == 3) {
-            ASSERT_STR_EQ(s, "bar");
-        }
-    });
-
-    cstring_array_destroy(array);
+TEST test_cstring_array_aligned_from_strings(void) {
+    cstring_array_aligned *array = cstring_array_aligned_from_strings((char *[]){"hello", "world"}, 2);
+    ASSERT_EQ(array->indices->n, 2);
+    ASSERT_EQ(array->str->n, 12);
+    ASSERT_EQ(array->str->a[11], '\0');
+    cstring_array_aligned_destroy(array);
     PASS();
 }
 
 SUITE(cstring_array_suite) {
     RUN_TEST(test_cstring_array_new);
+    RUN_TEST(test_cstring_array_aligned_new);
     RUN_TEST(test_cstring_array_new_size);
+    RUN_TEST(test_cstring_array_aligned_new_size);
     RUN_TEST(test_cstring_array_capacity);
+    RUN_TEST(test_cstring_array_aligned_capacity);
     RUN_TEST(test_cstring_array_used);
+    RUN_TEST(test_cstring_array_aligned_used);
     RUN_TEST(test_cstring_array_num_strings);
+    RUN_TEST(test_cstring_array_aligned_num_strings);
     RUN_TEST(test_cstring_array_resize);
+    RUN_TEST(test_cstring_array_aligned_resize);
     RUN_TEST(test_cstring_array_clear);
+    RUN_TEST(test_cstring_array_aligned_clear);
     RUN_TEST(test_cstring_array_start_token);
+    RUN_TEST(test_cstring_array_aligned_start_token);
     RUN_TEST(test_cstring_array_terminate);
+    RUN_TEST(test_cstring_array_aligned_terminate);
     RUN_TEST(test_cstring_array_add_string);
+    RUN_TEST(test_cstring_array_aligned_add_string);
     RUN_TEST(test_cstring_array_add_string_len);
+    RUN_TEST(test_cstring_array_aligned_add_string_len);
     RUN_TEST(test_cstring_array_cat_string);
+    RUN_TEST(test_cstring_array_aligned_cat_string);
     RUN_TEST(test_cstring_array_cat_string_len);
+    RUN_TEST(test_cstring_array_aligned_cat_string_len);
     RUN_TEST(test_cstring_array_from_char_array);
-    RUN_TEST(test_cstring_array_foreach);
+    RUN_TEST(test_cstring_array_aligned_from_char_array);
+    RUN_TEST(test_cstring_array_from_strings);
+    RUN_TEST(test_cstring_array_aligned_from_strings);
 }
-
-
 
 GREATEST_MAIN_DEFS();
 
